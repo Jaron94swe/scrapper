eac1da17e9df6701c4f8f2b011e8eb59c33a1983
Sven Larsson <ejlandeen@gmail.com>
2019-12-16 12:58:49 -0700

    intial commit, need to adjust code to work with the ksl.com

---
+++ public/app.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+    $('.modal').modal();
+});
+
+
+$.getJSON("/articles", function (data) {
+    for (var i = 0; i < data.length; i++) {
+        if (!data[i].image.includes("https://")) {
+            data[i].image = 'https://www.ksl.com/news/utah' + data[i].image;
+        } else {
+            console.log('full image link found!');
+        }
+        $("#articles").append("<article class='modal-trigger' data-target='modal1' data-id='" + data[i]._id + "'><img src=" + data[i].image + "><div class='clear'><p class='tag'>" + data[i].tag + "</p><h5>" + data[i].title + "</h5><br /><p>" + data[i].link + "</p></div></article>");
+    }
+});
+
+$(document).on("click", "article", function () {
+    $("#notes").empty();
+    // Save the id
+    var thisId = $(this).attr("data-id");
+
+    // Ajax call for the Article
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+    })
+        .then(function (data) {
+            // console.log(data);
+            $("#notes").append("<h5>" + data.title + "</h5>");
+            $("#notes").append("<input id='titleinput' placeholder='note title' name='title' >");
+            $("#notes").append("<textarea id='bodyinput' placeholder='note body' name='body'></textarea>");
+            $("#notes").append("<button class='waves-effect waves-light btn-small black white-text' data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+            if (data.note) {
+                // use map to display // looks like multiple notes save but with odd relationships to the article
+                $("#notes").append("<h6 class='center'>Existing Notes:</h6><hr class='hr-modal'><div id='stored-notes'></div>");
+                $("#stored-notes").append(data.note.title);
+                $("#stored-notes").append("<br>");
+                $("#stored-notes").append(data.note.body);
+                // console.log('this is data.note.id: ', data.note.id);
+                $("#stored-notes").append("<br><button id='delete-note' data-id="
+                    + data.note._id
+                    + " class='waves-effect waves-light btn-small black white-text'>delete note</button><hr class='hr-modal'>")
+            }
+        });
+});
+
+
+$(document).on("click", "#savenote", function () {
+    // grab the id 
+    var thisId = $(this).attr("data-id");
+
+    // POST request to change the note
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+            title: $("#titleinput").val(),
+            body: $("#bodyinput").val()
+        }
+    })
+
+        .then(function (data) {
+            console.log(data);
+            // display feedback to the user
+            $("#notes").text('note saved!');
+        });
+
+    // $("#titleinput").val("");
+    // $("#bodyinput").val("");
+});
+
+// route to delete notes:
+$(document).on("click", "#delete-note", function () {
+    // grab the id 
+    var thisId = $(this).attr("data-id");
+
+    // which method to delete?
+    $.ajax({
+        method: "POST",
+        url: "/notes/" + thisId,
+        data: {
+            title: $("#titleinput").val(),
+            body: $("#bodyinput").val()
+        }
+    })
+
+        .then(function (data) {
+            console.log(data);
+            // display feedback to the user
+            $("#notes").text('note deleted!');
+        });
+});
---
+++ server.js
@@ -0,0 +1,134 @@
+// require node packages: 
+var express = require("express");
+var exphbs  = require('express-handlebars');
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var path = require('path');
+
+// require mongoose models
+var db = require("./models");
+
+var PORT = process.env.PORT || 3004 ;
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.set('views', path.join(__dirname, 'views/'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+// connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/kslnews";
+mongoose.connect(MONGODB_URI, 
+  { useNewUrlParser: true, 
+  useUnifiedTopology: true, 
+  useFindAndModify: false }
+  ).then(() => {console.log('db connected')});
+
+// Routes================= //
+
+// GET route for scraping the freeCodeCamp news page
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.ksl.com/news/utah").then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    // grab every article tag, and do the following:
+    $(".queue_story").each(function(i, element) {
+      var result = {};
+
+      // Add the text, tag, href and image of every link, and save them as properties of the result object
+      result.title = $(this)
+        .find("h2").text().trim();
+        result.tag = $(this)
+        .find("span").text().trim();
+        // console.log('this is result.tag: ', result.tag);
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+      result.image = $(this)
+        .children("a")
+        .children("img").attr("src");
+
+      /* 
+      NOTE: for images hosted on freecodecamp that are not rendered with a full (external) url i.e. '/news/authors/images/dwayne-johnson.jpg',
+      there is logic included in app.js (line 16) to concatenate the missing part of the image url. 
+      */  
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // console.log(dbArticle.image);
+        })
+        .catch(function(err) {
+          // alert('Sorry! There\'s no new content to scrape!');
+          console.log('mongo !!ERROR!! :  ', err);
+        });
+    });
+
+    // display feedback
+    res.redirect('/');
+  });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  db.Article.find({}).sort({_id : -1})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+  })
+    .catch(function(error){
+      res.sendStatus(500);
+    })
+});
+
+// Route for grabbing a specific Article by id, populate it with note
+app.get("/articles/:id", function(req, res) {
+  db.Article.findOne({_id: req.params.id})
+    .populate("note")
+    .then(function (dbArticle){
+      res.json(dbArticle);
+    })
+    .catch( function(err){
+      if (err) {
+        res.sendStatus(500);
+      }
+    })
+  });
+
+
+// Saving/updating an Article's associated Note NEED TO UPDATE LOGIC FOR MANY NOTES
+app.post("/articles/:id", function(req, res) {
+db.Note.create(req.body)
+.then(function(dbNote){
+    return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+})
+.then(function(dbArticle) {
+  res.json(dbArticle);
+})
+.catch(function(err) {
+  res.json(err);
+});
+});
+
+// Delete an Article's associated Note // 
+app.post("/notes/:id", function(req, res) {
+  console.log('note id: ',req.params.id);
+   db.Note.deleteOne({ _id: req.params.id })
+   .then(function(dbNote) {
+     res.json(dbNote);
+   });
+  })
+
+
+app.listen(PORT, function() {
+  console.log("App running on localhost: " + PORT);
+});
