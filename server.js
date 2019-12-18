f9fb7e77b61bc59f980b0a377387bc3fcdbfa028
Sven Larsson <ejlandeen@gmail.com>
2019-12-18 11:59:53 -0700

    setup heroku with mongod in server.js file

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/', function (req, res) {
 });
 
 // connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/freeCodeCampNews";
+var MONGODB_URI = process.env.MONGODB_URI || "mongod://freeCodeCampNews:wrS!8B@z!R4WpNU@ds261440.mlab.com:61440/heroku_qnv4nc30";
 mongoose.connect(MONGODB_URI, 
   { useNewUrlParser: true, 
   useUnifiedTopology: true, 
